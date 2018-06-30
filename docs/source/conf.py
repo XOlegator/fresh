80ad009d29a3b5e0447737b8cd9e5e772e1d83c5
Oleg <subspam@mail.ru>
2018-06-30 21:43:32 +0300

    Test theme of docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -113,7 +113,8 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -287,3 +288,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# For default theme
+#html_style = 'RTD'
